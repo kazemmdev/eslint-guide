ff4e31bba662bba8911397d57372a89f3b764153
Kazem Mirzaei <k90mirzaei@gmail.com>
2023-03-13 17:22:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var exampleVaraible = "World";
+// let another_variable = "nothing";
+
+function print(value) {
+  const outputResults = "hello " + value + "!";
+  console.log(outputResults);
+}
+
+print(exampleVaraible);
